fda25de9cab9ae1f0ab0e5498e8f981f4a817e00
Nick42-beep <69941259+Nick42-beep@users.noreply.github.com>
2024-04-21 10:37:28 -0500

    finished adding functions to I2C2_Sensor.h

--- I2C2_Sensor.h
+++ I2C2_Sensor.h
@@ -20,7 +20,9 @@ uint32_t I2C_read();
 void I2C_repeated_start();
 void I2C_ACK();
 void I2C_NACK();
-
+void I2C_read_multiple_bytes_params(uint8_t reg, int numSamples);
+uint8_t I2C_read_with_params(uint8_t reg);
+uint8_t I2C_write_with_params(uint8_t reg, uint8_t val);
 #ifdef	__cplusplus
 }
 #endif
