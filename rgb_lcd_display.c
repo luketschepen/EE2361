fa9ebae8a03f6b8fdb87b583c396f8ae92afeb2e
colejaege <166561068+colejaege@users.noreply.github.com>
2024-04-23 16:46:47 -0500

    Add files via upload

--- rgb_lcd_display.c
+++ rgb_lcd_display.c
@@ -1,8 +1,13 @@
 /*
- * File:   grove-rgb.c
- * Author(s): 
- *
- * Created on April 9, 2024, 4:37 PM
+ * Date: 4/9/2024
+ * Name(s): Cole Jaeger, Dhayalan Balasubarmanian, Luke Tschepen, Nickk
+ * Project Name: Blood Oxygen Sensor
+ * Program Description: This program initializes the Grove RGB/LCD display
+ * to be compatible with the PIC24 using I2C communication. It contains various
+ * functions to manipulate and display different text characters and colors on
+ * the display.
+ * References/Code Citations: Arduino Grove RGB file, JHD data sheet
+ * 
  */
 
 #include "xc.h"
@@ -11,10 +16,9 @@
 #include "button_switch.h"
 //#include "I2C1.h"
 
-//uses c-ASM code to delay 1 millisecond
-
 //ARE WE USING THESE I2C functions IN HERE OR seperate file???
 void delay_ms(unsigned int ms){
+    //uses c-ASM code to delay 1 millisecond
     while(ms-- > 0){
         asm("repeat #15998");
         asm("nop");
@@ -43,7 +47,10 @@ void I2C_write(uint8_t data) {
 }
 
 void rgb_cmd(char Package) {
-    //Send START
+    // I2C communication protocol betweem the PIC24 and Grove LCD
+    // Used to send a command with data to the LCD 
+    // Package: an 8-bit command
+    
     I2C_start();
     
     I2C_write(LCD_SLAVE_ADDR);
@@ -56,15 +63,10 @@ void rgb_cmd(char Package) {
     
 }
 
-void rgb_write(char Package){
-    
-    I2C_write(WRITEBIT);
-    
-    I2C_write(Package);
-    
-}
-
 void setReg(unsigned char reg, unsigned char dat) {
+    //I2C protocol used to set the LED registers for the RGB display
+    //reg: desired LED register
+    //data: binary number representing a color from 0-255
     I2C_start();
     I2C_write(RGB_SLAVE_ADDR);
     I2C_write(reg);
@@ -73,57 +75,73 @@ void setReg(unsigned char reg, unsigned char dat) {
 }
 
 void setRGB(unsigned char r, unsigned char g, unsigned char b){
+    //calls the setReg function for each RGB color
+    //r: red value from 0-255
+    //g: green value from 0-255
+    //b: blue value from 0-255
     setReg(0x04, r);
     setReg(0x03, g);
     setReg(0x02, b);
 }
 
 void rgb_clr(){
+    //clears the text on the display
     rgb_cmd(DISPLAY_CLR);
     delay_ms(2000);
 }
 
 void rgb_home() {
+    //returns the cursor to the initial position (0,0)
     rgb_cmd(LCD_RETURNHOME);
     delay_ms(2000);
 }
 
 void noDisplay() {
+    //turns the display off
     rgb_cmd(DISPLAY_CONTROL);
 }
 
 void display() {
+    //turns the display on
     rgb_cmd(DISPLAY_CONTROL | LCD_DISPLAYON);
 }
 
 void blinkLED(void){
+    //flashes the specified LED registers
     setReg(0x07, 0x17);
     setReg(0x06, 0x7F);
 }
 
 void noBlinkLED(){
+    //stops the flashing of the LED registers
     setReg(0x07, 0x00);
     setReg(0x06, 0xFF);
 }
 
 void setColorAll() {
+    //turns all colors off
     setRGB(0,0,0);
 }
 
 void setColorWhite() {
+    //turns the screen white
     setRGB(255, 255, 255);
 }
 
 void setColorRed() {
+    //turns the screen red
     setRGB(255, 0, 0);
 }
 
 void setCursor(uint8_t x, uint8_t y){
+    //sets the cursor to the desired position on the screen
+    //x: column (0-15)
+    //y: row (0-1)
     rgb_cmd(0x0 | 0x80 | (0x40 * y + x));
 }
 
 void grovergb_init(void){
-    //initialization from Grove-LCD data-sheet
+    //initialization from Grove-LCD/RGB data-sheet and Arduino reference code
     delay_ms(50);//wait > 30ms
     
     rgb_cmd(FUNCTION_SET | LCD_8BITMODE | LCD_2LINE); // function set 0011NFXX N = line mode 1/2 (0/1) F = display off/on (0/1)
@@ -164,6 +182,8 @@ void grovergb_init(void){
 }
 
 void printStr(const char s[]){
+    //prints a string to the display
+    //s: an array of characters representing a string
     int len = strlen(s);
     
     I2C_start();
@@ -181,6 +201,8 @@ void printStr(const char s[]){
 }
 
 void printChar(char myChar){ 
+    //prints a character to the display
+    //myChar: a character
     I2C_start();
     I2C_write(LCD_SLAVE_ADDR);
     I2C_write(WRITEBIT);
